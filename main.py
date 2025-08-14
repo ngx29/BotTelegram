aa510ac191c8e45b4cd70f414337271ec7819216
Nicolas <102701619+ngx29@users.noreply.github.com>
2025-08-13 19:42:26 -0500

    Update main.py
    Quitando la implementación de 'imghdr'

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import os
 import tempfile
 import logging
 from flask import Flask, request, abort
-import sys
-import imghdr_pure as imghdr
-sys.modules['imghdr']=imghdr
 import telegram
 import openai
 from gtts import gTTS
